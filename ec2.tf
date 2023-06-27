d8a509aa41d2c92d12090565abaea4e2fc4cd463
Olumide <o3cloudng@gmail.com>
2023-06-27 09:58:13 +0100

    updated sonarqube installtion script

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,27 @@
 # configured aws provider with proper credentials
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = "us-east-1"
-  profile = "terraform-user"
+  profile = "terraform-ecs"
 }
 
 
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket  = "o3cloudng-terraform-state-bucket"
+    bucket  = "o3cloudng-terraform-state-bucket-1"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
-    profile = "terraform-user"
+    profile = "terraform-ecs"
   }
 }
 
@@ -40,18 +50,25 @@ resource "aws_default_subnet" "default_az1" {
 
 
 # create security group for the ec2 instance
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 80 and 22"
+resource "aws_security_group" "sonar_security_group" {
+  name        = "SG Sonarqube"
+  description = "allow access on ports 9000 and 22"
   vpc_id      = aws_default_vpc.default_vpc.id
 
   ingress {
-    description = "http access"
-    from_port   = 80
-    to_port     = 80
+    description = "http access 9000 for sonarqube"
+    from_port   = 9000
+    to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  # ingress {
+  #   description = "http access 8080 for Jenkins"
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
     description = "ssh access"
@@ -69,44 +86,74 @@ resource "aws_security_group" "ec2_security_group" {
   }
 
   tags = {
-    Name = "ec2 security group"
+    Name = "sonar security group"
   }
 }
 
 
 # use data source to get a registered amazon linux 2 ami
-data "aws_ami" "amazon_linux_2" {
+# data "aws_ami" "amazon_linux_2" {
+#   most_recent = true
+#   owners      = ["amazon"]
+
+#   filter {
+#     name   = "owner-alias"
+#     values = ["amazon"]
+#   }
+
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm*"]
+#   }
+# }
+
+# data "aws_ami" "ubuntu" {
+#     most_recent = true
+
+#     filter {
+#         name   = "name"
+#         values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+#     }
+
+#     filter {
+#         name   = "virtualization-type"
+#         values = ["hvm"]
+#     }
+
+#     owners = ["099720109477"] # Canonical
+# }
+data "aws_ami" "ubuntu-linux-2004" {
   most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
+  owners      = ["099720109477"] # Canonical
+ 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-*"]
+  }
+ 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 
 # launch the ec2 instance and install website
 resource "aws_instance" "ec2_instance" {
-  ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.ubuntu-linux-2004.id
+  instance_type          = "t2.medium"
   subnet_id              = aws_default_subnet.default_az1.id
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
-  user_data              = file("install_techmax.sh")
+  vpc_security_group_ids = [aws_security_group.sonar_security_group.id]
+  key_name               = "terraform-key-pair"
+  user_data              = "${file("install_sonarqube.sh")}"
 
   tags = {
-    Name = "techmax server"
+    Name = "Sonarqube server"
   }
 }
 
 
 # print the url of the server
 output "ec2_public_ipv4_url" {
-  value = join("", ["http://", aws_instance.ec2_instance.public_ip])
+  value = join("", ["http://", aws_instance.ec2_instance.public_ip, ":9000"])
 }
--- install_sonarqube.sh
+++ install_sonarqube.sh
@@ -1,32 +1,34 @@
 #! /bin/bash
+sudo apt update -y
 
-# Ensure that your software packages are up to date on your instance by using the following command to perform a quick software update:
-sudo yum update –y
+sudo apt upgrade -y 
 
-sudo yum upgrade
+# Install Java Developer Kits (JDK)
+sudo apt install openjdk-17-jre -y
 
-#  Install unzip
-yum install unzip -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
 
-# Create a user soarqube
-adduser sonarqube
+# Install Jenkins
+sudo apt-get install jenkins -y
 
-# Download sonarqube
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
+# First, I Install Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-# Unzip sonarqube
-unzip *
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Change mode and permissions
-chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
-# Change ownership to sonarqube user
-chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424
+sudo apt update -y
 
-# Change directory into the linux soanrqube distribution
-cd sonarqube-9.4.0.54424/bin/linux-x86-64/
+apt-cache policy docker-ce -y
 
-# Start sonarqube
-./sonar.sh start
+sudo apt install docker-ce -y
 
+#  Next, I will run sonarqube from image
 
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
